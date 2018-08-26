95c5bfe38104b6df6946bb174c407ca3de1ec8c1
Maks <ush.maksim@gmail.com>
2018-08-26 12:34:47 +0300

    Add start data for quiz and quiz filters

---
+++ data/dbquiz.js
@@ -0,0 +1,130 @@
+module.exports = {
+  areas: [
+    {
+      title: "Web Frontend",
+      option: "web-front",
+    },
+    {
+      title: "Web Backend",
+      option: "web-back",
+    },
+    {
+      title: "Mobile",
+      option: "mobile",
+    },
+    {
+      title: "Desktop",
+      option: "desktop",
+    }
+  ],
+  languages: [
+    {
+      title: "JavaScript",
+      option: "js",
+      area: [
+        "web-front",
+        "web-back",
+        "mobile",
+        "desktop",
+      ]
+    },
+    {
+      title: "PHP",
+      option: "php",
+      area: [
+        "web-back",
+      ]
+    },
+    {
+      title: "Swift",
+      option: "swift",
+      area: [
+        "mobile",
+      ]
+    },
+    {
+      title: "Python",
+      option: "python",
+      area: [
+        "web-back",
+        "desktop",
+      ]
+    },
+    {
+      title: "C#",
+      option: "csharp",
+      area: [
+        "web-back",
+        "desktop"
+      ]
+    },
+  ],
+  frameworks: [
+    {
+      title: "Express",
+      option: "express",
+      language: "js",
+      area: "web-back",
+    },
+    {
+      title: "Laravel",
+      option: "laravel",
+      language: "php",
+      area: "web-back",
+    },
+    {
+      title: "Symfony",
+      option: "symfony",
+      language: "php",
+      area: "web-back",
+    },
+    {
+      title: "ASP.NET",
+      option: "aspnet",
+      language: "csharp",
+      area: "web-back",
+    },
+    {
+      title: "ASP.NET",
+      option: "aspnet",
+      language: "csharp",
+      area: "web-back",
+    },
+    {
+      title: "Anguar",
+      option: "angular",
+      language: "js",
+      area: "web-front",
+    },
+    {
+      title: "Vue",
+      option: "vue",
+      language: "js",
+      area: "web-front",
+    },
+    {
+      title: "Django",
+      option: "django",
+      language: "python",
+      area: "web-back",
+    },
+    {
+      title: "Kivy",
+      option: "kivy",
+      language: "python",
+      area: "desktop",
+    },
+    {
+      title: "PyQt",
+      option: "pyqt",
+      language: "python",
+      area: "desktop",
+    },
+    {
+      title: "Electron",
+      option: "electron",
+      language: "js",
+      area: "desktop",
+    },
+  ],
+}
