d468ec276170064386346d66e857e84204918f7c
Maks <ush.maksim@gmail.com>
2018-08-26 11:54:51 +0300

    Add inutial data for quote

---
+++ data/dbquote.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    content: "Everything Begins With An Idea.",
+    author: "Earl Nightengale", 
+  },
+  {
+    content: "Find A Group Of People Who Challenge And Inspire You, Spend A Lot Of Time With Them, And It Will Change Your Life.",
+    author: "Amy Poehler", 
+  },
+  {
+    content: "In Order To Carry A Positive Action We Must Develop Here A Positive Vision.",
+    author: "Dalai Lama", 
+  },
+  {
+    content: "The Art Of Communication Is The Language Of Leadership.",
+    author: "James Humes", 
+  },
+  {
+    content: "A Year From Now You Will Wish You Had Started Today.",
+    author: "Karen Lamb", 
+  },
+  {
+    content: "Innovation distinguishes between a leader and a follower.",
+    author: "Steve Jobs", 
+  },
+]
